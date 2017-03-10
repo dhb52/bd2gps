b18f6652f7d5ba06440e545c929f918222b79e84
dhb52 <dhb52@126.com>
2017-03-10 17:44:36 +0800

    fix unicode bug

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # bd2gps
 坐标转换工具 PyQt界面封装
 
+# build
+## python2 + PyQt4
+> py -3 -m venv .venv
+> .venv\Scripts\activate.bat
+
+install PyQt4, PyInstaller
+
+> pyuic4 ui_main.ui -o ui_main4.py
+> pyinstaller bd2gps.py -w -i app.ico --add-data "app.ico;."
+
+
+## python3 + PyQt5
+> py -3 -m venv .venv
+> .venv\Scripts\activate.bat
+> pip install -r requestments.txt
+> pyuic5 ui_main.ui -o ui_main5.py
+> python cx_freeze.py build
+
 #转换算法
 https://github.com/wandergis/coordTransform_py
 
--- bd2gps.py
+++ bd2gps.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from ui_main import Ui_Dialog
+
 try:
     from PyQt5.QtWidgets import (QDialog, QApplication)
+    from ui_main5 import Ui_Dialog
+    def unicode(s):
+        return s
 except ImportError:
     from PyQt4.QtGui import (QDialog, QApplication)
+    from ui_main4 import Ui_Dialog
 
 from convertor import (
     bd09togcj02,
@@ -30,7 +34,7 @@ class MainDlg(QDialog, Ui_Dialog):
 
     def start_convert(self):
         self.textEditDst.clear()
-        src = self.textEditSrc.toPlainText()
+        src = unicode(self.textEditSrc.toPlainText())
         lines = src.splitlines()
         for line in lines:
             try:
